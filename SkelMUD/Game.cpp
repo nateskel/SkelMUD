1ab77b854575f8a6042479f21a04027e5f0fad6d
Nate Skelton <skeltonn@gmail.com>
2015-04-19 19:26:27 -0700

    Rewrote command processing architecture

--- SkelMUD/Game.cpp
+++ SkelMUD/Game.cpp
@@ -10,6 +10,9 @@
 
 Game::Game()
 {
+	DIRECTIONS = { "NORTH", "N", "SOUTH", "S", "EAST", "E", "WEST", "W", "NORTHEAST", "NE", "NORTHWEST",
+		"NW", "SOUTHEAST", "SE", "SOUTHWEST", "SW", "UP", "U", "DOWN", "D" };
+	std::sort(DIRECTIONS.begin(), DIRECTIONS.end());
 	m_running = false;
 #ifdef _WIN32
 	m_mutex = CreateMutex(NULL, false, NULL);
@@ -21,6 +24,7 @@ Game::Game()
 	File planetFile = File();
 	//m_accounts = accountFile.LoadAccounts();
 	m_planets = planetFile.LoadPlanets();
+	registerCommands();
 }
 
 Game::~Game()
@@ -28,32 +32,39 @@ Game::~Game()
 
 }
 
-void Game::RegisterCommand(std::string command_string, void* command_function)
+void Game::registerCommands()
 {
-
+	RegisterCommand("USERNAME", processUsername, Connection::USERNAME);
+	RegisterCommand("PASSWORD", processPassword, Connection::PASSWORD);
+	RegisterCommand("SAY", processSay, Connection::LOGGEDIN);
+	RegisterCommand("LOOK", processLook, Connection::LOGGEDIN);
+	RegisterCommand("L", processLook, Connection::LOGGEDIN);
+	RegisterCommand("DIRECTION", processDirectionalCommand, Connection::LOGGEDIN);
+	RegisterCommand("OOC", processOOC, Connection::LOGGEDIN);
+	RegisterCommand("OOCON", processOOC, Connection::OOC);
+	RegisterCommand("QUIT1459", processQuit, Connection::LOGGEDIN);
+	RegisterCommand("HELP", processHelp, Connection::LOGGEDIN);
 }
 
-void Game::ProcessCommand(std::string command_string)
+void Game::augmentCommand(Connection::State state, std::string &data)
 {
-
-}
-
-void Game::AddOutput(int id, std::string output)
-{
-	std::map<int, std::string>::iterator iterator = m_output_map.find(id);
-	if (iterator != m_output_map.end())
-		iterator->second.append(output);
-	else
-		m_output_map[id] = output;
-}
-
-std::string Game::GetOutput(int id)
-{
-	std::map<int, std::string>::iterator iterator = m_output_map.find(id);
-	if (iterator != m_output_map.end())
-		return iterator->second;
-	else
-		return NULL;
+	if (state == Connection::USERNAME)
+	{
+		data.insert(0, "USERNAME ");
+	}
+	else if (state == Connection::PASSWORD)
+	{
+		data.insert(0, "PASSWORD ");
+	}
+	else if (state == Connection::OOC)
+	{
+		data.insert(0, "OOCON ");
+	}
+	else if (isDirection(data))
+	{
+		data = Tokenizer::UpperCase(data);
+		data.insert(0, "DIRECTION ");
+	}
 }
 
 void Game::Start()
@@ -69,7 +80,6 @@ void Game::Start()
 		it = m_connection_map.begin();
 		it_end = m_connection_map.end();
 		Thread::Unlock(m_mutex);
-		//int DEBUGINT = 0;
 		for (it; it != it_end; )
 		{
 			int id = it->first;
@@ -84,29 +94,11 @@ void Game::Start()
 				Player* player = m_player_map[id];
 				if (player == NULL)
 				{
-					// Log.Error("Logged in player not found!");
+					Log.Error("Logged in player not found!");
 				}
 				else
 				{
-					std::string statusbar = RED;
-					statusbar.append("<");
-					statusbar.append("HP: ");
-					statusbar.append(std::to_string(player->GetHP()));
-					statusbar.append("/");
-					statusbar.append(std::to_string(player->GetMaxHP()));
-					statusbar.append(CYAN);
-					statusbar.append(" SP: ");
-					statusbar.append(std::to_string(player->GetSP()));
-					statusbar.append("/");
-					statusbar.append(std::to_string(player->GetMaxSP()));
-					statusbar.append(YELLOW);
-					statusbar.append(" Stamina: ");
-					statusbar.append(std::to_string(player->GetStamina()));
-					statusbar.append("/");
-					statusbar.append(std::to_string(player->GetMaxStamina()));
-					statusbar.append(">");
-					statusbar.append(WHITE);
-					statusbar.append("\r\n> ");
+					std::string statusbar = createStatusBar(player);
 					connection->Send(statusbar);
 				}
 			}
@@ -138,39 +130,14 @@ void Game::Start()
 				{
 					std::string data = connection->GetNextData();
 					Utils::RemoveEndline(data);
-					switch (state)
+					augmentCommand(state, data);
+					if (!ProcessCommand(data, state, id))
 					{
-					case Connection::USERNAME:
-						//TODO: this should be run through character creation
-						// the name used here is the username, not the character name (TEMPORARY)
-						m_player_map[id] = new Player(id, data);
-						m_planets[m_player_map[id]->GetPlanetID()]->GetRoom(m_player_map[id]->GetRoomID())->AddPlayer(m_player_map[id]);
-						m_sender.Send("Password: ", connection, YELLOW);
-						connection->SetState(Connection::PASSWORD);
-						break;
-					case Connection::PASSWORD:
-						m_sender.Send("Logged in!\r\n", connection);
-						connection->SetState(Connection::LOGGEDIN);
-						processLook(id);
-						break;
-					case Connection::LOGGEDIN:
-						processCommand(data, id);
-						break;
-					case Connection::OOC: {
-						std::vector<std::string> allTokens = Tokenizer::GetAllTokens(data);
-						if (allTokens.size() == 2) {
-							if (Tokenizer::UpperCase(allTokens[0]) == "OOC" && Tokenizer::UpperCase(allTokens[1]) == "OFF") {
-								m_sender.Send("OOC Off\r\n", m_connection_map[id], MAGENTA);
-								m_connection_map[id]->SetState(Connection::LOGGEDIN);
-								break;
-							}
-						}
-						data.insert(0, "OOC ");
-						processCommand(data, id);
+						m_sender.Send("Unrecognized Command. Type 'help' for a list of commands.\r\n", connection);
 					}
-						break;
-					default:
-						break;
+					else
+					{
+						m_sender.Send("", connection);
 					}
 				}
 				it++;
@@ -180,116 +147,99 @@ void Game::Start()
 	}
 }
 
-void Game::processCommand(std::string data, int id)
+void Game::RegisterCommand(std::string command_string, void* (*command_function)(int, std::string, Game*), Connection::State state)
 {
-	std::string command = Tokenizer::UpperCase(Tokenizer::GetFirstToken(data));
-	std::vector<std::string> commandlist = Tokenizer::GetAllTokens(data);
-	if (command == "OOC")
-	{
-		if (commandlist.size() == 0)
-			return;
-		if (Tokenizer::UpperCase(commandlist[0]) == "ON")
-		{
-			m_sender.Send("OOC On\r\n", m_connection_map[id], MAGENTA);
-			m_connection_map[id]->SetState(Connection::OOC);
-			return;
-		}
-		std::string output = "\r\n";
-		output.append(m_player_map[id]->GetName());
-		output.append("(ooc): ");
-		output.append(data);
-		output.append("\r\n");
-		m_sender.SendAll(output, m_connection_map, id, YELLOW);
-	}
-	else if (command == "SAY")
-	{
-		Player* player = m_player_map[id];
-		int planet_id = player->GetPlanetID();
-		int room_id = player->GetRoomID();
-		std::vector<int> players = m_planets[planet_id]->GetRoom(room_id)->GetPlayerIDs(id);
-		std::string color = BOLD;
-		color.append(CYAN);
-		std::string prefix = "\r\n";
-		prefix.append(m_player_map[id]->GetName());
-		prefix.append(": ");
-		data.insert(0, prefix);
-		data.append("\r\n");
-		m_sender.SendToMultiple(data, m_connection_map, players, color);
-	}
-	else if (command == "HELP")
-	{
-		m_sender.Send("Commands are not case sensitive\r\n\r\nOOC <message>: Out of character global chat\r\nOOC <ON/OFF>: Turn OOC permanently ON/OFF\r\nSAY <message>: In character local chat.\r\n", m_connection_map[id]);
-	}
-	else if (command == "QUIT1459")
-	{
-		m_running = false;
-	}
-	else if (command == "LOOK" || command == "L")
-	{
-		processLook(id);
-	}
-	else if (command == "CON")
-	{
-		m_sender.Send(std::to_string(m_connection_map.size()), m_connection_map[id]);
-	}
-	else
-	{
-		if(!processDirectionalCommand(command, id))
-			m_sender.Send("Unrecognized Command. Type 'help' for a list of commands.\r\n", m_connection_map[id]);
-	}
+	Command command;
+	command.command_function = command_function;
+	command.state = state;
+	m_command_map[command_string] = command;
 }
 
-bool Game::processDirectionalCommand(std::string command, int id)
+bool Game::ProcessCommand(std::string command_string, Connection::State state, int id)
+{
+	std::string command_input = Tokenizer::UpperCase(Tokenizer::GetFirstToken(command_string));
+	if (command_input.size() == 0)
+		return false;
+	if (m_command_map.find(command_input) == m_command_map.end())
+		return false;
+	Command command = m_command_map[command_input];
+	if (command.state == state)
+	{
+		command.command_function(id, command_string, this);
+		return true;
+	}
+	return false;
+}
+
+void Game::AddOutput(int id, std::string output)
+{
+	std::map<int, std::string>::iterator iterator = m_output_map.find(id);
+	if (iterator != m_output_map.end())
+		iterator->second.append(output);
+	else
+		m_output_map[id] = output;
+}
+
+std::string Game::GetOutput(int id)
+{
+	std::map<int, std::string>::iterator iterator = m_output_map.find(id);
+	if (iterator != m_output_map.end())
+		return iterator->second;
+	else
+		return NULL;
+}
+
+void* Game::processDirectionalCommand(int id, std::string command, Game* game)
 {
 	bool result = false;
-	Player* current_player = m_player_map[id];
+	Player* current_player = game->m_player_map[id];
 	int current_planet = current_player->GetPlanetID();
 	int current_room = current_player->GetRoomID();
 	if (command == "N" || command == "NORTH")
-		result = m_planets[current_planet]->MoveNorth(current_room, id);
+		result = game->m_planets[current_planet]->MoveNorth(current_room, id);
 	else if (command == "S" || command == "SOUTH")
-		result = m_planets[current_planet]->MoveSouth(current_room, id);
+		result = game->m_planets[current_planet]->MoveSouth(current_room, id);
 	else if (command == "E" || command == "EAST")
-		result = m_planets[current_planet]->MoveEast(current_room, id);
+		result = game->m_planets[current_planet]->MoveEast(current_room, id);
 	else if (command == "W" || command == "WEST")
-		result = m_planets[current_planet]->MoveWest(current_room, id);
+		result = game->m_planets[current_planet]->MoveWest(current_room, id);
 	else if (command == "NE" || command == "NORTHEAST")
-		result = m_planets[current_planet]->MoveNorthEast(current_room, id);
+		result = game->m_planets[current_planet]->MoveNorthEast(current_room, id);
 	else if (command == "NW" || command == "NORTHWEST")
-		result = m_planets[current_planet]->MoveNorthWest(current_room, id);
+		result = game->m_planets[current_planet]->MoveNorthWest(current_room, id);
 	else if (command == "SE" || command == "SOUTHEAST")
-		result = m_planets[current_planet]->MoveSouthEast(current_room, id);
+		result = game->m_planets[current_planet]->MoveSouthEast(current_room, id);
 	else if (command == "SW" || command == "SOUTHWEST")
-		result = m_planets[current_planet]->MoveSouthWest(current_room, id);
+		result = game->m_planets[current_planet]->MoveSouthWest(current_room, id);
 	else if (command == "U" || command == "UP")
-		result = m_planets[current_planet]->MoveUp(current_room, id);
+		result = game->m_planets[current_planet]->MoveUp(current_room, id);
 	else if (command == "D" || command == "DOWN")
-		result = m_planets[current_planet]->MoveDown(current_room, id);
+		result = game->m_planets[current_planet]->MoveDown(current_room, id);
 	if (!result)
 	{
-		m_sender.Send("Can't go that way!\r\n", m_connection_map[id]);
+		game->m_sender.Send("Can't go that way!\r\n", game->m_connection_map[id]);
 	}
 	else
 	{
-		std::vector<int> leaving_players = m_planets[current_planet]->GetRoom(current_room)->GetPlayerIDs(id);
-		std::vector<int> arriving_players = m_planets[current_planet]->GetRoom(current_player->GetRoomID())->GetPlayerIDs(id);
+		std::vector<int> leaving_players = game->m_planets[current_planet]->GetRoom(current_room)->GetPlayerIDs(id);
+		std::vector<int> arriving_players = game->m_planets[current_planet]->GetRoom(current_player->GetRoomID())->GetPlayerIDs(id);
 		std::string arrive_message = "\r\n";
 		arrive_message.append(current_player->GetName());
 		arrive_message.append(" entered the room\r\n");
 		std::string leave_message = "\r\n";
 		leave_message.append(current_player->GetName());
 		leave_message.append(" left the room\r\n");
-		m_sender.SendToMultiple(arrive_message, m_connection_map, arriving_players);
-		m_sender.SendToMultiple(leave_message, m_connection_map, leaving_players);
-		processLook(id);
+		game->m_sender.SendToMultiple(arrive_message, game->m_connection_map, arriving_players);
+		game->m_sender.SendToMultiple(leave_message, game->m_connection_map, leaving_players);
+		processLook(id, "", game);
 	}
-	return result;
+	return 0;
 }
 
-void Game::processLook(int id)
+void* Game::processLook(int id, std::string data, Game* game)
 {
-	Player* current_player = m_player_map[id];
-	Room* room = m_planets[current_player->GetPlanetID()]->GetRoom(current_player->GetRoomID());
+	Player* current_player = game->m_player_map[id];
+	Room* room = game->m_planets[current_player->GetPlanetID()]->GetRoom(current_player->GetRoomID());
 	std::string output = room->GetLongDescription();
 	output.append("\r\n");
 	output.append(room->GetShortDescription());
@@ -299,7 +249,7 @@ void Game::processLook(int id)
 	std::vector<int> visible_players = room->GetPlayerIDs(id);
 	for (int i = 0; i < (int)visible_players.size(); i++)
 	{
-		output.append(m_player_map[visible_players[i]]->GetName());
+		output.append(game->m_player_map[visible_players[i]]->GetName());
 		output.append(" is here.\r\n");
 	}
 	output.append(WHITE);
@@ -325,7 +275,79 @@ void Game::processLook(int id)
 	if (room->GetDown() != -1)
 		output.append("Down ");
 	output.append("\r\n");
-	m_sender.Send(output, m_connection_map[id]);
+	game->m_sender.Send(output, game->m_connection_map[id]);
+	return 0;
+}
+
+void* Game::processUsername(int id, std::string data, Game* game)
+{
+	game->m_player_map[id] = new Player(id, data);
+	game->m_planets[game->m_player_map[id]->GetPlanetID()]->GetRoom(game->m_player_map[id]->GetRoomID())->AddPlayer(game->m_player_map[id]);
+	game->m_sender.Send("Password: ", game->m_connection_map[id], YELLOW);
+	game->m_connection_map[id]->SetState(Connection::PASSWORD);
+	return 0;
+}
+
+void* Game::processPassword(int id, std::string data, Game* game)
+{
+	Connection* connection = game->m_connection_map[id];
+	game->m_sender.Send("Logged in!\r\n", connection);
+	connection->SetState(Connection::LOGGEDIN);
+	processLook(id, "", game);
+	return 0;
+}
+
+void* Game::processSay(int id, std::string data, Game* game)
+{
+	Player* player = game->m_player_map[id];
+	int planet_id = player->GetPlanetID();
+	int room_id = player->GetRoomID();
+	std::vector<int> players = game->m_planets[planet_id]->GetRoom(room_id)->GetPlayerIDs(id);
+	std::string color = BOLD;
+	color.append(CYAN);
+	std::string prefix = "\r\n";
+	prefix.append(game->m_player_map[id]->GetName());
+	prefix.append(": ");
+	data.insert(0, prefix);
+	data.append("\r\n");
+	game->m_sender.SendToMultiple(data, game->m_connection_map, players, color);
+	return 0;
+}
+
+void* Game::processOOC(int id, std::string data, Game* game)
+{
+	std::vector<std::string> commandlist = Tokenizer::GetAllTokens(data);
+	if (commandlist.size() == 0)
+		return 0;
+	if (commandlist.size() == 2 && commandlist[0] == "OOC" && Tokenizer::UpperCase(commandlist[1]) == "OFF")
+	{
+		game->m_sender.Send("OOC Off\r\n", game->m_connection_map[id], MAGENTA);
+		game->m_connection_map[id]->SetState(Connection::LOGGEDIN);
+		return 0;
+	}
+	if (Tokenizer::UpperCase(commandlist[0]) == "ON")
+	{
+		game->m_sender.Send("OOC On\r\n", game->m_connection_map[id], MAGENTA);
+		game->m_connection_map[id]->SetState(Connection::OOC);
+		return 0;
+	}
+	std::string output = "\r\n";
+	output.append(game->m_player_map[id]->GetName());
+	output.append("(OOC): ");
+	output.append(data);
+	output.append("\r\n");
+	game->m_sender.SendAll(output, game->m_connection_map, id, YELLOW);
+	return 0;
+}
+
+void* Game::processHelp(int id, std::string data, Game* game)
+{
+	game->m_sender.Send("Commands are not case sensitive\r\n\r\nOOC <message>: Out of character global chat\r\nOOC <ON/OFF>: Turn OOC permanently ON/OFF\r\nSAY <message>: In character local chat.\r\n", game->m_connection_map[id]);
+}
+
+void* Game::processQuit(int id, std::string data, Game* game)
+{
+	game->m_running = false;
 }
 
 THREAD Game::ListenThread(LPVOID lpParam)
@@ -347,4 +369,36 @@ THREAD Game::ListenThread(LPVOID lpParam)
 	game->m_running = false;
 	myListener.Close();
 	return 0;
+}
+
+bool Game::isDirection(std::string input)
+{
+	std::string command = Tokenizer::GetFirstToken(input);
+	command = Tokenizer::UpperCase(command);
+	input.insert(0, command);
+	return std::binary_search(DIRECTIONS.begin(), DIRECTIONS.end(), command);
+}
+
+std::string Game::createStatusBar(Player* player)
+{
+	std::string statusbar = RED;
+	statusbar.append("<");
+	statusbar.append("HP: ");
+	statusbar.append(std::to_string(player->GetHP()));
+	statusbar.append("/");
+	statusbar.append(std::to_string(player->GetMaxHP()));
+	statusbar.append(CYAN);
+	statusbar.append(" SP: ");
+	statusbar.append(std::to_string(player->GetSP()));
+	statusbar.append("/");
+	statusbar.append(std::to_string(player->GetMaxSP()));
+	statusbar.append(YELLOW);
+	statusbar.append(" Stamina: ");
+	statusbar.append(std::to_string(player->GetStamina()));
+	statusbar.append("/");
+	statusbar.append(std::to_string(player->GetMaxStamina()));
+	statusbar.append(">");
+	statusbar.append(WHITE);
+	statusbar.append("\r\n> ");
+	return statusbar;
 }
--- SkelMUD/Game.h
+++ SkelMUD/Game.h
@@ -8,6 +8,7 @@
 #include "Planet.h"
 #include "Thread.h"
 #include "Utils.h"
+#include "Logger.h"
 #include <map>
 
 class Game
@@ -18,26 +19,44 @@ public:
 	void Start();
 	void AddOutput(int id, std::string output);
 	std::string GetOutput(int id);
-	void RegisterCommand(std::string command_string, void* command_function);
-	void ProcessCommand(std::string command_string);
+	struct Command
+	{
+		void* (*command_function)(int, std::string, Game*);
+		Connection::State state;
+	};
+	void RegisterCommand(std::string command_string, void* (*command_function)(int, std::string, Game*), Connection::State state);
+	bool ProcessCommand(std::string command_string, Connection::State state, int id);
 
 private:
+	std::vector<std::string> DIRECTIONS;
 	bool m_running;
 	HANDLE m_mutex;
+	Logger Log = Logger(Logger::L_ERROR);
 	std::map<SOCKET, Connection*> m_connection_map;
 	std::map<int, Player*> m_player_map;
 	std::vector<Planet*> m_planets;
 	std::vector<Connection::Account> m_accounts;
+	std::vector<Command> m_command_list;
 	std::map<int, std::string> m_output_map;
-	std::map<std::string, void*> m_command_map;
+	std::map<std::string, Command> m_command_map;
 	static THREAD ListenThread(LPVOID lpParam);
 	Sender m_sender;
 	OutputManager m_output_manager;
 
 	void processCommand(std::string command, int id);
-	bool processDirectionalCommand(std::string command, int id);
-	void processLook(int id);
-	//virtual void recv_callback(char* data, int socket);
+	void registerCommands();
+	std::string createStatusBar(Player* player);
+	bool isDirection(std::string input);
+	void augmentCommand(Connection::State state, std::string &data);
+
+	static void* processLook(int id, std::string data, Game* game);
+	static void* processUsername(int id, std::string data, Game* game);
+	static void* processPassword(int id, std::string data, Game* game);
+	static void* processSay(int id, std::string data, Game* game);
+	static void* processDirectionalCommand(int id, std::string command, Game* game);
+	static void* processOOC(int id, std::string data, Game* game);
+	static void* processHelp(int id, std::string data, Game* game);
+	static void* processQuit(int id, std::string data, Game* game);
 };
 
 #endif // GAME_H
