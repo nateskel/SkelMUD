6047ae579f1a5166c7f5f4b4380d23ed07707812
nate <skeltonn@gmail.com>
2019-08-02 15:31:11 -0700

    refactor

---
+++ SkelMUD/Mixin.cpp
@@ -0,0 +1,5 @@
+//
+// Created by drax on 8/1/19.
+//
+
+#include "Mixin.h"
---
+++ SkelMUD/Mixin.h
@@ -0,0 +1,14 @@
+//
+// Created by drax on 8/1/19.
+//
+
+#ifndef SKELMUD_MIXIN_H
+#define SKELMUD_MIXIN_H
+
+
+class Mixin {
+
+};
+
+
+#endif //SKELMUD_MIXIN_H
